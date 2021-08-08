a6f91b6dc8875bc74e1c57ecdfc9507934339389
Ilya Kuklin <ilyakuklin43@gmail.com>
2021-08-08 15:46:56 +0300

    ClientFields

--- MiniCRMServer/MiniCRMCore/Areas/Clients/Models/Client.cs
+++ MiniCRMServer/MiniCRMCore/Areas/Clients/Models/Client.cs
@@ -8,12 +8,45 @@ namespace MiniCRMCore.Areas.Clients.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 
+		public string LegalEntitiesNames { get; set; }
+		public string DomainNames { get; set; }
+		public string Contact { get; set; }
+		public string Diagnostics { get; set; }
+		
 		public virtual List<Offer> Offers { get; set; }
 
 		public class Dto
 		{
 			public int Id { get; set; }
+
+			/// <summary>
+			/// Название
+			/// </summary>
 			public string Name { get; set; }
+
+			/// <summary>
+			/// Названия юрлиц.
+			/// </summary>
+			public string LegalEntitiesNames { get; set; }
+
+			/// <summary>
+			/// Названия сайтов и доменов.
+			/// </summary>
+			public string DomainNames { get; set; }
+
+			/// <summary>
+			/// Контактное лицо.
+			/// </summary>
+			public string Contact { get; set; }
+
+			/// <summary>
+			/// Диагностика клиента.
+			/// </summary>
+			public string Diagnostics { get; set; }
+
+			/// <summary>
+			/// Список КП клиента.
+			/// </summary>
 			public List<Offer.Dto> Offers { get; set; }
 		}
 	}
---
+++ MiniCRMServer/MiniCRMCore/Migrations/20210808124245_ClientFields.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniCRMCore.Migrations
+{
+    public partial class ClientFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Diagnostics",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DomainNames",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LegalEntitiesNames",
+                table: "Clients",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Diagnostics",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DomainNames",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "LegalEntitiesNames",
+                table: "Clients");
+        }
+    }
+}
