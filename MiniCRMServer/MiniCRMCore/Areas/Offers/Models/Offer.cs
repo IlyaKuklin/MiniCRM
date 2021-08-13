331915483df3919cafea49f547f5c0378891b9c0
Ilya Kuklin <ilyakuklin43@gmail.com>
2021-08-13 09:34:50 +0300

    Offer EditDto fields

--- MiniCRMServer/MiniCRMCore/Areas/Offers/Models/Offer.cs
+++ MiniCRMServer/MiniCRMCore/Areas/Offers/Models/Offer.cs
@@ -73,12 +73,38 @@ namespace MiniCRMCore.Areas.Offers.Models
 			public string OfferCase { get; set; }
 
 			public string Description { get; set; }
+
+			/// <summary>
+			/// Суть предложения
+			/// </summary>
 			public string OfferPoint { get; set; }
+
+			/// <summary>
+			/// Рекомендации клиенту
+			/// </summary>
 			public string Recommendations { get; set; }
+
+
+			/// <summary>
+			/// Прочая документация
+			/// </summary>
 			public string OtherDocumentation { get; set; }
+
+			/// <summary>
+			/// Сопроводительное письмо
+			/// </summary>
 			public string CoveringLetter { get; set; }
+
+			/// <summary>
+			/// Аналогичный кейсы
+			/// </summary>
 			public string SimilarCases { get; set; }
+
+			/// <summary>
+			/// Новости
+			/// </summary>
 			public string NewsLinks { get; set; }
+
 			public OfferPotential Potential { get; set; }
 			public string Stage { get; set; }
 
@@ -94,8 +120,43 @@ namespace MiniCRMCore.Areas.Offers.Models
 			public string ProductSystemType { get; set; }
 			public string BriefIndustryDescription { get; set; }
 			public string OfferCase { get; set; }
+			public string Description { get; set; }
+
+			/// <summary>
+			/// Суть предложения
+			/// </summary>
+			public string OfferPoint { get; set; }
+
+			/// <summary>
+			/// Рекомендации клиенту
+			/// </summary>
+			public string Recommendations { get; set; }
+
+
+			/// <summary>
+			/// Прочая документация
+			/// </summary>
+			public string OtherDocumentation { get; set; }
+
+			/// <summary>
+			/// Сопроводительное письмо
+			/// </summary>
+			public string CoveringLetter { get; set; }
+
+			/// <summary>
+			/// Аналогичный кейсы
+			/// </summary>
+			public string SimilarCases { get; set; }
 
 			public int ClientId { get; set; }
+
+			/// <summary>
+			/// Новости
+			/// </summary>
+			public string NewsLinks { get; set; }
+
+			public OfferPotential Potential { get; set; }
+			public string Stage { get; set; }
 		}
 	}
 
