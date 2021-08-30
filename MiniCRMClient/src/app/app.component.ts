98d8d02b3f1e95a2357696af18556dfc928e23c7
Ilya Kuklin <ilyakuklin43@gmail.com>
2021-08-30 16:40:45 +0300

    get showToolbar()

--- MiniCRMClient/src/app/app.component.ts
+++ MiniCRMClient/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/services/auth.service';
 
 @Component({
   selector: 'mcrm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'MiniCRMClient';
+  title = 'Автомат Сервис CRM';
+
+  constructor(private readonly authService: AuthService) {}
+
+  get showToolbar(): boolean {
+    const userData = this.authService.getUserData();
+    const uuidRegex =
+      /\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b/;
+    const isOfferView = uuidRegex.test(window.location.href) && !userData;
+    return !isOfferView;
+  }
 }
