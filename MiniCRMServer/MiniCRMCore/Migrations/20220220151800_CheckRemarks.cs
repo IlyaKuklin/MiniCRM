a8da668b10a9d9384d48ab2ce8e500fc0dea50f9
Ilya Kuklin <ilyakuklin43@gmail.com>
2022-02-21 13:15:27 +0300

    CheckRemarks : Migration

---
+++ MiniCRMServer/MiniCRMCore/Migrations/20220220151800_CheckRemarks.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniCRMCore.Migrations
+{
+    public partial class CheckRemarks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CheckRemark",
+                table: "OfferRules",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CheckStatus",
+                table: "OfferRules",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckRemark",
+                table: "OfferRules");
+
+            migrationBuilder.DropColumn(
+                name: "CheckStatus",
+                table: "OfferRules");
+        }
+    }
+}
